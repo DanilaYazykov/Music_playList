494dac04341ca744e976523309dacd6c438958e4
DanilaYazykov <danila_yazykov@mail.ru>
2022-11-25 09:15:51 +0300

    Sprint eight

---
+++ app/src/main/java/com/example/playlist_maker_2022/SearchingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.playlist_maker_2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searching)
+    }
+
+}
---
+++ app/src/main/java/com/example/playlist_maker_2022/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.playlist_maker_2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}

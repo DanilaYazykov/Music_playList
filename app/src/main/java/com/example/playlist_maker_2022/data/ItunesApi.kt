334504b9239abb41ab90c1a1d0c4758c3492d598
DanilaYazykov <111272559+DanilaYazykov@users.noreply.github.com>
2023-04-13 14:37:07 +0300

    15 cпринт

--- app/src/main/java/com/example/playlist_maker_2022/data/ItunesApi.kt
+++ app/src/main/java/com/example/playlist_maker_2022/data/ItunesApi.kt
@@ -4,11 +4,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-/*interface ItunesApi {
-    @GET("/search?entity=song")
-    fun search(@Query("term") text: String): Call<TrackResponse>
-}*/
-
 interface ItunesApi2 {
     @GET("/search?entity=song")
     suspend fun search(@Query("term") text: String): Response<TrackResponse>

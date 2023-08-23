3125e79cef616cda0d6171ccbb996f9991c49363
DanilaYazykov <111272559+DanilaYazykov@users.noreply.github.com>
2023-08-23 12:44:01 +0200

    Glide начал с ума сходить, чтобы не плодить классы @GlideModule, добавлен listner.

--- app/src/main/java/com/example/playlist_maker_2022/presentation/ui/media/playLists/PlaylistHolder.kt
+++ app/src/main/java/com/example/playlist_maker_2022/presentation/ui/media/playLists/PlaylistHolder.kt
@@ -1,13 +1,18 @@
 package com.example.playlist_maker_2022.presentation.ui.media.playLists
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.target.Target
 import com.example.playlist_maker_2022.R
 import com.example.playlist_maker_2022.domain.models.Playlists
 
@@ -25,6 +30,26 @@ class PlaylistHolder(itemView: View, val context: Context) : RecyclerView.ViewHo
             .load(playlist.playlistImage)
             .placeholder(R.drawable.no_reply)
             .transform(CenterCrop(), RoundedCorners(8))
+            .addListener(object : com.bumptech.glide.request.RequestListener<android.graphics.drawable.Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Log.e("Glide", "Error loading image", e)
+                    return true
+                }
+                override fun onResourceReady(
+                    resource: Drawable,
+                    model: Any,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+            })
             .into(image)
     }
 
